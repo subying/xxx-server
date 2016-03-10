1f0fc17b85db8f8b3ff5607112bed80dc23aefe6
yJs <tearlight2008@gmail.com>
2016-03-11 00:00:06 +0800

    修改router写法

--- libs/controller.js
+++ libs/controller.js
@@ -1,28 +1,49 @@
 /*
 	controller
  */
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
 
-const controllerRule = {
+const _controllerPath = '../controllers/';
+
+//控制器规则
+var controllerRule = {
 	'/': 'index',
 	'/:id': 'index'
 };
 
-function controller(router){
-	
-	router.get('/',function *(next){
-		var controllerPad = require('../controllers/index');
-		yield controllerPad.bind(this)();
-		
-		yield next;
-	});
+var controllerMain = {
+	init: (router)=>{
+		var _self = controllerMain;
+		//循环规则 设置路由
+		_.forIn(controllerRule,(value,name)=>{
+			router.get(name, function *(next){
+				//读取控制器
+				var controller = _self.getContrller(value);
+				yield controller.bind(this)();
 
-	router.get('/:id',function *(next){
-		var controllerPad = require('../controllers/index');
-		yield controllerPad.bind(this)();
-		
-		
-		yield next;
-	});
-}
+				yield next;
+			});
+		});
+	},
+	/*
+	 * @description 获取控制器
+	 */
+	getContrller: (name)=>{
+		var _path = _controllerPath+name+'.js';
 
-module.exports = controller;
+		//转换文件绝对路径 
+		var filePath = path.resolve(__dirname,_path);
+
+		//判断文件是否存在
+		if(fs.existsSync(filePath)){
+			return require(_path);
+		}else{
+			throw Error('controller is not exists');
+		}
+	}
+};
+
+
+module.exports = controllerMain.init;
--- libs/router.js
+++ libs/router.js
@@ -18,7 +18,8 @@ module.exports = (app)=>{
 
 		//如果状态不是200 则表示没有控制器匹配成功
 		if(this.status != 200){
-
+			this.status = 404;
+			this.body = '404';
 		}
 
 		yield next;
