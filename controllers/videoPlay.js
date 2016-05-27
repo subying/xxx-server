d603cbf802318acc93c275b295156c30db463fd1
subying <tearlight2008@gmail.com>
2016-05-28 00:55:56 +0800

    add play

--- controllers/videoPlay.js
+++ controllers/videoPlay.js
@@ -7,7 +7,6 @@ var cache = require('../libs/cache');
 module.exports = function *(){
     var _id = 'videoPid'+this.params.id;
     var playData = yield cache.get(_id);
-    console.log(_id,playData);
     if(playData && playData.hd){
         var options = {
             url: playData.hd,
@@ -16,6 +15,7 @@ module.exports = function *(){
                 'Range': this.headers['range']
             }
         };
+        console.log(playData.hd);
         var x = request(options);
         this.body = x;
     }
